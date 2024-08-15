e56f5624172829c3aae15fa93f8a714ee8b06002
simay-uygur <simayuygur09@gmail.com>
2024-08-15 12:04:22 +0300

    @Pathvariable

--- src/main/java/com/simayuygur/springbootlearning/controller/ThymeleafController.java
+++ src/main/java/com/simayuygur/springbootlearning/controller/ThymeleafController.java
@@ -3,6 +3,7 @@ package com.simayuygur.springbootlearning.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,12 +12,30 @@ public class ThymeleafController {
     @GetMapping("/thymeleaf1")
     @ResponseBody
     public String getThymeleaf1() {
-        return "thymeleaf1 ";
+        return "thymeleaf1";
     }
 
     @GetMapping("/thymeleaf2")
     public String getThymeleaf2(Model model) {
         model.addAttribute("key_model", "I came from the model ");
-        return "thymeleaf2 ";
+        return "thymeleaf2";
     }
+
+    //forgot to write productdto and initialize with a builder
+    @GetMapping({"/", "thymeleaf3"}) //index
+    public String getThymeleaf3(Model model) {
+        model.addAttribute("key_model2", "how are you");
+        return "thymeleaf3";
+    }
+
+    @GetMapping({"/hymeleaf4", "/thymeleaf4/{id}"}) //index
+    public String getThymeleaf4(Model model , @PathVariable(name = "id", required = false) Long id) {
+        if(id != null) {
+            model.addAttribute("key_model4", "how are you this website's id is  " +id);
+        }
+        model.addAttribute("key_model2", "welcome to thymeleaf4");
+        return "thymeleaf4";
+    }
+
+
 }
