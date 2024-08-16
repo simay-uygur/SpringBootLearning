c8bbe506842e73a5d513a7b20b2e639531d79a3f
simay-uygur <simayuygur09@gmail.com>
2024-08-16 12:11:45 +0300

    PasswordEncoderBean (inactive)

---
+++ src/main/java/com/simayuygur/springbootlearning/bean/PasswordEncoderBean.java
@@ -0,0 +1,13 @@
+package com.simayuygur.springbootlearning.bean;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderBean {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoderBean() {
+//        return new BCryptPasswordEncoder();c
+//    }
+}
