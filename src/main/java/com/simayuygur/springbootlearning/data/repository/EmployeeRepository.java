32c2da62652a5a993227a052ce10e3279778f3fc
simay-uygur <simayuygur09@gmail.com>
2024-08-17 16:28:10 +0300

    EmployeeRepository

--- src/main/java/com/simayuygur/springbootlearning/data/repository/EmployeeRepository.java
+++ src/main/java/com/simayuygur/springbootlearning/data/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.simayuygur.springbootlearning.data.repository;
 
+import com.simayuygur.springbootlearning.data.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepository > CrudRepository
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface  EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
