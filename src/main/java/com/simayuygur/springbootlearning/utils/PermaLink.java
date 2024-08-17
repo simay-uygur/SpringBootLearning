3a4b0cfdd618a4f02074b3540be510f7450a86be
simay-uygur <simayuygur09@gmail.com>
2024-08-17 21:16:28 +0300

    updating PermaLink

--- src/main/java/com/simayuygur/springbootlearning/utils/PermaLink.java
+++ src/main/java/com/simayuygur/springbootlearning/utils/PermaLink.java
@@ -2,4 +2,7 @@ package com.simayuygur.springbootlearning.utils;
 
 public class PermaLink {
     //GITHUB: https://github.com/simay-uygur/SpringBootLearning
+    //SWAGGER: http://localhost:8080/swagger-ui/index.html
+    //H2-console: http://localhost:8080/h2-console/
+    //JSON online parser: https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
 }
