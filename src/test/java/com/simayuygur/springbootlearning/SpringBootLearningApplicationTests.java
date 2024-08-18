acb5cbce577c217cf6dff1b5a8d01946e0fa47df
simay-uygur <simayuygur09@gmail.com>
2024-08-18 20:59:04 +0300

    Writing unit tests

--- src/test/java/com/simayuygur/springbootlearning/SpringBootLearningApplicationTests.java
+++ src/test/java/com/simayuygur/springbootlearning/SpringBootLearningApplicationTests.java
@@ -1,13 +1,53 @@
 package com.simayuygur.springbootlearning;
 
+import com.simayuygur.springbootlearning.data.entity.EmployeeEntity;
+import com.simayuygur.springbootlearning.data.repository.EmployeeRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import test.TestCrud;
 
 @SpringBootTest
-class SpringBootLearningApplicationTests {
+class  SpringBootLearningApplicationTests implements TestCrud {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Override
+    public void testCreate() {
+        EmployeeEntity employeeEntity = EmployeeEntity
+                .builder()
+                .name("Simay TEST")
+                .surname("Uygur TEST")
+                .emailId("emil@test.com")
+                .build();
+        employeeRepository.save(employeeEntity);
+        assertNotNull(employeeRepository.findById(1L).get());
+    }
+
+    @Override
+    public void testUpdate() {
+
+    }
+
+    @Override
+    public void testDelete() {
+
+    }
+
+    @Override
+    public void testList() {
+
+    }
+
+    @Override
+    public void testFindById() {
+
+    }
 }
---
+++ src/test/java/test/TestCrud.java
@@ -0,0 +1,12 @@
+package test;
+
+public interface TestCrud {
+    //TEST CRUD
+
+
+    public void testCreate();
+    public void testUpdate();
+    public void testDelete();
+    public void testList();
+    public void testFindById();
+}
